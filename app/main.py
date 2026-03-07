b3a079c68338807b62adb84b3865b1276a5f8713
edenis00 <edenis0072@gmail.com>
2026-03-07 08:56:12 +0100

    feat: add route for middleware testing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.middleware.context import UserContextMiddleware
 from app.api.v1.auth import router as auth_router
 
@@ -12,3 +12,8 @@ app.include_router(auth_router, prefix="/api/v1")
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the AutoPedicare API!"}
+
+
+@app.get("/debug-middleware")
+async def debug_middleware(request: Request):
+    return request.state.context
